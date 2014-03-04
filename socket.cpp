121b3e91f0be8dd77faa58627e42f1ae860bdf6f
Paul Rademacher <paul@tastylabs.com>
2014-03-04 01:27:06 -0800

    Class Socket

---
+++ socket.cpp
@@ -0,0 +1,22 @@
+#include "socket.hpp"
+
+Socket::Socket(boost::asio::io_service& io_service) {
+  socket_.reset(new tcp::socket(io_service));
+}
+
+tcp::socket& Socket::get_raw_socket() {
+  return *socket_;
+}
+
+size_t Socket::read_until(boost::asio::streambuf& buffer,
+    const std::string& delim, boost::system::error_code& error_code) {
+  return boost::asio::read_until(*socket_, buffer, std::string("\r\n\r\n"), error_code);
+}
+
+Socket::~Socket() {
+  if (socket_) {
+    boost::system::error_code ignored_error;
+    socket_->shutdown(tcp::socket::shutdown_both, ignored_error);
+    socket_->close();
+  }
+}
---
+++ socket.hpp
@@ -0,0 +1,25 @@
+#ifndef __HTTP_SOCKET_H__
+#define __HTTP_SOCKET_H__
+
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+class Socket {
+public:
+  explicit Socket(boost::asio::io_service& io_service);
+
+  ~Socket();
+
+  size_t read_until(boost::asio::streambuf& buffer,
+    const std::string& delim, boost::system::error_code& error_code);
+
+  tcp::socket& get_raw_socket();
+
+private:
+  std::unique_ptr<tcp::socket> socket_;
+};
+
+typedef std::shared_ptr<Socket> SocketSharedPtr;
+
+#endif // __HTTP_SOCKET_H__
